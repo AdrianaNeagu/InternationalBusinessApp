2626acf65cf104980110ecf1c346dc7ae722dd0a
AdrianaNeagu <adriana.neagu86@gmail.com>
2019-01-07 21:00:36 +0200

    add layouts (recycler view + recycler item)

--- app/src/main/java/com/example/adriana/internationalbusinessapp/activity/MainActivity.java
+++ app/src/main/java/com/example/adriana/internationalbusinessapp/activity/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.adriana.internationalbusinessapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.adriana.internationalbusinessapp.R;
+import com.example.adriana.internationalbusinessapp.model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Transaction> transactionList = new ArrayList<>();
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.transactions_recycler_view);
     }
 }
